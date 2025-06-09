290c76fbddc0e54fcc1d5dfcb34b7107137b9ffe
JordanDev <ariasjordan943@gmail.com>
2025-06-09 18:23:49 -0400

    Configuracion de librerias y dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import * as $ from 'jquery'; // Import jQuery para usar en el proyecto
+import 'bootstrap'; // Import Bootstrap para usar en el proyecto
+
+// Hacer jQuery disponible globalmente
+(window as any).$ = $;
+(window as any).jQuery = $;
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
