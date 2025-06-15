76b27dc1fe3b62f65b1c6a4f5502670a3d6e6c56
JordanDev <ariasjordan943@gmail.com>
2025-06-15 15:12:17 -0400

    Creacion e implementación de Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { Routes } from '@angular/router';
 //COMPONENTES
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
-
+import { Home } from './components/home/home';
 //RUTAS
 export const routes: Routes = [
+  { path: 'home',component: Home },
   { path: 'login',component: Login },
   { path: 'register',component: Register },
-  //ruta por defecto
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  //Manejo de rutas no encontradas
-  { path: '**', redirectTo: '/login' }
+
+    //ruta por defecto
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    //Manejo de rutas no encontradas
+    { path: '**', redirectTo: '/home' }
+  
 ];
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.model';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
+import { NgForm,FormsModule } from '@angular/forms';
+import { error } from 'jquery';
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsModule, CommonModule, HttpClientModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home implements OnInit{
+  public title:any;
+
+  constructor(
+  ){
+    this.title= 'Bienvenido a NGSocial';
+  }
+
+  ngOnInit(): void {
+      console.log('home.Component cargado!!');
+      
+  }
+}
