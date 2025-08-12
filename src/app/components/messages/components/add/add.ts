2ee21f222ccee37f329715b99d1b12acd81c6f91
JordanDev <ariasjordan943@gmail.com>
2025-08-12 18:28:29 -0400

    Creacion del nuevo componente received

--- src/app/components/messages/components/add/add.ts
+++ src/app/components/messages/components/add/add.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
+import { UserService } from '../../../../services/user.service';
+import { User } from '../../../../models/user.model';
+import { Follow } from '../../../../models/follow.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { global } from '../../../../services/global';
+import { FollowService } from '../../../../services/follow.service';
+import { Sidebar } from '../../../sidebar/sidebar'; 
+import { error } from 'jquery';
 
 @Component({
   selector: 'app-add',
@@ -6,6 +18,15 @@ import { Component } from '@angular/core';
   templateUrl: './add.html',
   styleUrl: './add.css'
 })
-export class Add {
+export class Add implements OnInit{
+  public title:string;
+
+  constructor(){
+    this.title = 'Enviar mensaje';
+  }
+
+  ngOnInit(){
+    console.log('add.comnponent cargado...');
+  }
 
 }
